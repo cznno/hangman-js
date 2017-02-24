624b56b0e9f5c3fec04f7ce18318d33c46b63cee
smeraldo-ash <smeraldo.ash@gmail.com>
2017-02-24 13:38:30 +0800

    update

--- src/single_player.js
+++ src/single_player.js
@@ -1,50 +1,65 @@
 //single player mode
-//-------initialization--------
+
+//-------raise a question--------
 var key;
 var quest;
-var t="";
-var correct=0;
-var passed=0;
-$("#input").on('keypress', function (e) {
-    var gsChar=$("#input").val();
-    if (e.keyCode == 13 && gsChar!="" ) {
-        if(gsChar.charCodeAt(0)>97){
-            gsChar=gsChar.toUpperCase();
+var t = '';
+var correct = 0;
+var passed = 0;
+var chance = 0;
+
+function raiseQuest() {
+    var questIndex = Math.ceil(Math.random() * dict.length);
+    key = 'HELLO';
+    quest = key.replace(/./g, '*');
+    dict.slice(questIndex, 1);
+    chance=key.length;
+    $('span').eq(0).text(quest);    
+}
+
+//-------input--------
+    
+$('#input').on('keypress', function (e) {
+    if ((e.key >= 'A' && e.key <= 'z')) {
+        if (e.key>='a') {
+            $('input').val(e.key.toUpperCase());
         }
-        $("#input").val("");
-        postAGuess(gsChar);
-    }else if(e.keyCode>=65 && e.keyCode<=122){
-        return;
     }else{
         return false;
     }
 });
+$('#input').on('keyup', function (e) {
+    var gsChar = $('#input').val();
+    //console.log(e.keyCode);
+    if (e.keyCode === 13 && gsChar != '') {
+        $('#input').val('');
+        console.log(quest);
+        postAGuess(gsChar);
+    }
+});
 
-//-------raise a question--------
+//---------make guess--------
 
-function raiseQuest(){
-    var questIndex=Math.ceil(Math.random()*dict.length);
-    key="HELLO";
-    quest=key.replace(/./g,"*");
-    dict.slice(questIndex,1);
-    $("span").eq(0).text(quest);
-}
-
-function postAGuess(gsChar){
-    t+=gsChar;
-    var reg=new RegExp("[^"+t+"]","g");
-    quest=key.replace(reg,"*");
-    var reg2=new RegExp("\\w{"+key.length+"}","g");
-    if(reg2.test(quest)){
-        //$("span").eq(1).text(totalWordAllowed-res.data.totalWordCount);
+function postAGuess(gsChar) {
+    t += gsChar;
+    var reg = new RegExp('[^' + t + ']', 'g');
+    var isRight=quest;
+    quest = key.replace(reg, '*');
+    if(isRight===quest){
+        chance--;
+        console.log(chance);
+    }
+    var reg2 = new RegExp('\\w{' + key.length + '}', 'g');
+    if (reg2.test(quest)) {
+        //$('span').eq(1).text(totalWordAllowed-res.data.totalWordCount);
         correct++;
-        $("span").eq(2).text(correct);
-        $("span").eq(3).text(passed);
+        $('span').eq(2).text(correct);
+        $('span').eq(3).text(passed);
         raiseQuest();
     };
-    $("span").eq(0).text(quest);
+    $('span').eq(0).text(quest);
 }
 
-function nextQuest(){
+function nextQuest() {
     alert();
 }
